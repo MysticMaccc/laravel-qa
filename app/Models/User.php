c07f095ea083de4d88164f0519694e20a4f6148b
MysticMaccc <Cosmicsher96@gmail.com>
2024-01-03 14:36:58 +0800

    Add Author Info and Question Creation Date

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,6 +49,10 @@ class User extends Authenticatable
         return $this->hasMany(Question::class);
     }
 
-    
+    public function getUrlAttribute()
+    {
+        // return route("question.show", $this->id);
+        return '#';
+    }
 
 }
